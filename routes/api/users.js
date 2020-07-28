8bce4eff3e3da17fa131ac1c115c35c2a9e09b68
jitsumacmaster <jitsu.macmaster@gmail.com>
2020-07-28 16:42:20 -0700

    add proxy: http://localhost:5000 to frontend package json

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,12 +12,12 @@ const validateLoginInput = require('../../validation/login');
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.json({
-      id: req.user.id,
-      handle: req.user.handle,
-      email: req.user.email
-    });
-  })
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+})
 
 router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
@@ -26,32 +26,32 @@ router.post('/register', (req, res) => {
     return res.status(400).json(errors);
   }
 
-    // Check to make sure nobody has already registered with a duplicate email
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (user) {
-          // Throw a 400 error if the email address already exists
-          return res.status(400).json({email: "A user has already registered with this address"})
-        } else {
-          // Otherwise create a new user
-          const newUser = new User({
-            handle: req.body.handle,
-            email: req.body.email,
-            password: req.body.password
-          })
+  // Check to make sure nobody has already registered with a duplicate email
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Throw a 400 error if the email address already exists
+        return res.status(400).json({email: "A user has already registered with this address"})
+      } else {
+        // Otherwise create a new user
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        })
 
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser.save()
-                .then(user => res.json(user))
-                .catch(err => console.log(err));
-            })
-          })
-        }
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
       })
+    }
   })
+})
 
 
   router.post('/login', (req, res) => {
