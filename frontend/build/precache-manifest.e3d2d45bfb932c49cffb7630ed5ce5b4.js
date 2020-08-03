90344ef605c8fa093f1d17b7e60ee76c77dc37af
jitsumacmaster <jitsu.macmaster@gmail.com>
2020-08-03 09:51:28 -0700

    Build build

---
+++ frontend/build/precache-manifest.e3d2d45bfb932c49cffb7630ed5ce5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67306f756ba2f9d0c97fd05a7308158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "898b1c67cb878cd50c31",
+    "url": "/static/css/main.f465104c.chunk.css"
+  },
+  {
+    "revision": "e879431bfcb4d389014d",
+    "url": "/static/js/2.db1d1a5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db1d1a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898b1c67cb878cd50c31",
+    "url": "/static/js/main.f76868dd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
