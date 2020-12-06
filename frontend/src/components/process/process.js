bdf7c7c79e5e2964b81ed480ec2a6272224ad65b
jitsumacmaster <jitsu.macmaster@gmail.com>
2020-12-06 12:46:51 -0800

    Styling

--- frontend/src/components/process/process.js
+++ frontend/src/components/process/process.js
@@ -5,15 +5,15 @@ class Process extends React.Component {
   render() {
     return (
         <section id="process">
-        <h2>Our Process</h2>
-        <h3>Why Telemedicine?</h3>
-        <p>Trans-specific healthcare delivered via telemedicine has important advantages for transgender patients who may not have easy access to knowledgeable providers near their homes. The tools of telemedicine, including not only videoconferencing but also online lab test requisitions and results delivery, long-distance electronic pharmacy access, electronic record-keeping and legal access across state lines are now sufficiently advanced to make telehealth specialization in transgender medicine practical across much of the U.S. Trans-specific healthcare delivered via telemedicine also offers convenience, safety and privacy. Importantly, all of our Providers are trans themselves, thus allowing for more comfortable visits discussing issues that are often difficult to relate to for some other healthcare providers.</p>
-        <ul>
-            <li>Anyone with a smartphone, laptop, tablet or desktop computer will be able to apply through the patient portal and receive telemedicine based care.</li>
-            <li>Laboratory tests will be ordered through one of several national labs with local blood draw facilities, and prescriptions can be sent directly to your pharmacy from TransClinique.</li>
-            <li>Although the majority of your visits will be virtual, our practice plans to staff pop-up clinics in major cities throughout the year, where we hope to give you an opportunity to meet the TransClinique team in person.</li>
-        </ul>
-    </section>
+            <h2>Our Process</h2>
+            <h3>Why Telemedicine?</h3>
+            <p>Trans-specific healthcare delivered via telemedicine has important advantages for transgender patients who may not have easy access to knowledgeable providers near their homes. The tools of telemedicine, including not only videoconferencing but also online lab test requisitions and results delivery, long-distance electronic pharmacy access, electronic record-keeping and legal access across state lines are now sufficiently advanced to make telehealth specialization in transgender medicine practical across much of the U.S. Trans-specific healthcare delivered via telemedicine also offers convenience, safety and privacy. Importantly, all of our Providers are trans themselves, thus allowing for more comfortable visits discussing issues that are often difficult to relate to for some other healthcare providers.</p>
+            <ul>
+                <li>Anyone with a smartphone, laptop, tablet or desktop computer will be able to apply through the patient portal and receive telemedicine based care.</li>
+                <li>Laboratory tests will be ordered through one of several national labs with local blood draw facilities, and prescriptions can be sent directly to your pharmacy from TransClinique.</li>
+                <li>Although the majority of your visits will be virtual, our practice plans to staff pop-up clinics in major cities throughout the year, where we hope to give you an opportunity to meet the TransClinique team in person.</li>
+            </ul>
+        </section>
     );
   }
 }
