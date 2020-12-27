028a862e480e954971d4fe0e550ff5dc500f3b6d
Elizabeth Camporeale <ccamporeale@gmail.com>
2020-12-26 17:25:35 -0800

    update build with static

---
+++ frontend/build/precache-manifest.57131d0449426c011f94f60b96fc3676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c5306c523faef3415bf2aa14949496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1a774db77cff3324999",
+    "url": "/static/css/main.d9bed39a.chunk.css"
+  },
+  {
+    "revision": "4f940f5371774b9c0bdd",
+    "url": "/static/js/2.0a60e512.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.0a60e512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a774db77cff3324999",
+    "url": "/static/js/main.5288023c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
