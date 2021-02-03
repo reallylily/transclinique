9784b49ff140fbcbbb029acbd6a7c29c3db74c20
jitsumacmaster <jitsu.macmaster@gmail.com>
2021-02-03 13:36:46 -0800

    Edit text on Apt page

--- frontend/src/components/appointments/appointments.js
+++ frontend/src/components/appointments/appointments.js
@@ -18,12 +18,12 @@ class Appointments extends React.Component {
     return (
     <section id="appointments">
         <h2>Appointments</h2>
-        <h3>How To Set Up Your First Appointment:</h3>
-        <p>The first step to become a patient with TransClinique is to send us an email with your name, phone number and the email address that you prefer to have on record with us. You can fill out the form on the right hand side of this page to book an appointment. If you have any questions, please email us at <a href="mailto:alexispetramd@gmail.com">AlexisPetraMD@gmail.com</a>.</p>
-        <p>Once your self-referral is received, TransClinique will send you an email back with an active link to our HIPAA compliant, safe and secure patient portal. There you will fill out a registration form and make your first appointment with us. </p>
+        <p>Please fill out the form on the right hand side of this page to book an appointment. If this is your first appointment with us, please include your Date of Birth in the notes at checkout. If you have any questions, please email us at <a href="mailto:alexispetramd@gmail.com">AlexisPetraMD@gmail.com</a>.</p>
 
         {/* Square Appointments Embed Container */}
         <div className="square-appointments-embed-container">&nbsp;</div>
+
+        
     </section>
     );
   }
