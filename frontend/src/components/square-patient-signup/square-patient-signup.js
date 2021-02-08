115b6e11148e3fa8709f285282f6dc031fc3cb29
jitsumacmaster <jitsu.macmaster@gmail.com>
2021-02-08 11:43:50 -0800

    Add Allie and Casey Photos

---
+++ frontend/src/components/square-patient-signup/square-patient-signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SquarePatientSignup extends React.Component {
+
+  render() {
+    return (
+        <section id="square-patient-signup">
+
+        </section>
+    );
+  }
+}
+
+export default SquarePatientSignup;
