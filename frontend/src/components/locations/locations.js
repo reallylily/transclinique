78b409450e4f865ad260d0b5555dc54cd84917fc
jitsumacmaster <jitsu.macmaster@gmail.com>
2021-03-04 12:40:46 -0800

    Add Michigan to active states in locations

--- frontend/src/components/locations/locations.js
+++ frontend/src/components/locations/locations.js
@@ -7,9 +7,9 @@ class Locations extends React.Component {
         
     <section id="locations">
             <h2>Locations</h2>
-            <p>Currently serving all of Arizona, California, Colorado, Illinois, Nevada, New York and Washington State via telemedicine</p>
+            <p>Currently serving all of Arizona, California, Colorado, Illinois, Michigan, Nevada, New York and Washington State via telemedicine</p>
             <h3>Coming Soon</h3>
-            <p>Florida, Michigan, Nevada and Texas</p>
+            <p>Florida and Texas</p>
             <p>We expect to provide pop up clinics in San Francisco, Los Angeles, San Diego, Phoenix, Chicago and New York City to give patients opportunities to have in person appointments, as well as informal social gatherings to meet other members of the community.</p>
     </section>
     );
