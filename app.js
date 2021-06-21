639cb7317b02d56367e8a8a7cfc41855eae73c51
Lily Reale <lily.macmaster@palmetto.com>
2021-06-21 10:18:28 -0700

    update to run on yarn

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
