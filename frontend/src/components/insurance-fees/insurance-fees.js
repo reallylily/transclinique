9f245562e87ddf27693ba5a0f40fd84ae279df37
Lily Reale <lily.macmaster@palmetto.com>
2021-06-27 17:10:54 -0700

    update fees and insurance

--- frontend/src/components/insurance-fees/insurance-fees.js
+++ frontend/src/components/insurance-fees/insurance-fees.js
@@ -10,23 +10,45 @@ class InsuranceFees extends React.Component {
         <div id="fees">
             <h3>Fee Structure</h3>
             <p>
-                New Patient Evaluation for HRT $150 <br/>                                    
-                Existing Patient Evaluation for HRT $125 <br/>
-                Urgent and Primary Care Evaluation  $125 <br/>
-                Gender Name Change Letter $100
+                Our aim is to make TransClinique affordable to all . There will be three tiered services depending 
+                on what is best for your individual situation. 
             </p>
-        </div>
-        <div id="labs">
-            <h3>Labs</h3>
-            <p>There will be no charge on our end. Your insurance will likely cover your labs. Without insurance it will generally cost roughly $99 per visit.</p>
-        </div>
-        <div id="medications">
-            <h3>Medications</h3>
-            <p>Costs vary per prescription type and the pharmacy that you choose.</p>
+            <h3>
+            Bronze $59.99
+            </h3>
+            <p>
+            $59.99 a month will include all of your appointments for the year. Prescribing meds, ordering 
+            of labs, writing referral letters , PrEP and HIV counseling also included.  This is best for someone who has been on HRT for a while or has insurance to cover the cost of the labs already. In addition you will be part of the TransClinique events and have access to one  complimentary fitness session  and/or make up and styling class. 
+            </p>
+            <h3>
+            Sliver $79.99
+            </h3>
+            <p>
+            $79.99 a month covers all of the Bronze features except that TransClinique covers the cost 
+            of your labs. You also receive three month  access to live group fitness and/or make up and styling instructional classes. Lastly, Special VIP invites to TransClinique events  and trans coaching will be available to you. . 
+            </p>
+            <h3>
+            Gold $99.99
+            </h3>
+            <p>
+            $99.99 a month gets you all of the benefits of the silver package however with this premium 
+            service you can really take control of your transition from A to Z. Firstly unlimited appointments , 
+            texts and emails will be available to you the same day, essentially 24/7. In addition to the free labs, 
+            an emphasis on trans coaching and a dedicated coach will be available to you to make your transition as 
+            smooth as possible. I will personally introduce you to the top surgical and voice specialists in the 
+            field when you're ready and go over name change documents with you .  You will have unlimited access 
+            to all fitness and styling weekly events as well as a personalized diet plan to help you reach your 
+            health goals. This is best for those truly dedicated to fast tracking their transition goals and 
+            improving their overall health and fitness,
+            </p>
         </div>
         <div id="insurance">
             <h3>Insurance</h3>
-            <p>We are working diligently to get on multiple insurance plans however we are a cash only clinic at this early juncture. Even if the visit fee does not get reimbursed at this point, your plan will likely still cover the labs and medications. We will also create an insurance invoice which you can submit to your insurance company for reimbursement.</p>
+            <p>
+                If you have existing insurance it very likely will cover the cost of the medications we 
+                prescribe and sometimes the labs. If your labs are not covered under your insurance plan then 
+                I recommend joining the TransClinique silver or gold plan as it will be the most affordable for you.
+            </p>
         </div>
     </section>
     );
--- frontend/src/components/services/services.js
+++ frontend/src/components/services/services.js
@@ -9,18 +9,25 @@ class Services extends React.Component {
         <h2>Our Services</h2>
         <h3>What We Do</h3>
         <ul>
+        Our Services 
             <li>HRT</li>
-        	<li>Urgent care</li> 
-            <li>Basic Primary Care</li>
-	        <li>Documentation for legal name changes</li>
 	        <li>PREP and HIV testing</li>
+        	<li>ED meds</li> 
+            <li>Letters for name change and surgical referrals</li>
+	        <li>Trans Life Coaching and help with "next steps" including personalized introduction to specialists</li>
 	        <li>Customized holistic health routine including: 
 	            <ul>
-	                <li>Diet and exercise plan</li>
-	                <li>Trans life coaching</li>
-	                <li>Skincare routine and makeup application</li>
+                    <li>
+                    Live virtual group fitness instruction 
+                    </li>
+                    <li>
+                    Live virtual group Make up and Styling services 
+                    </li>
 	            </ul>
 	        </li>
+            <li>
+                Community Building with live in person events and big brother big sister chat functions among TransClinique members 
+            </li>
         </ul>
         <hr/>
     </section>
